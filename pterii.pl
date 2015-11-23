8a40fa594d14387cd8bab42ed5dc4b5a21b89aca
KeyboardFire <andy@keyboardfire.com>
2015-11-22 21:58:02 -0600

    Implement +-*/ sa

--- pterii.pl
+++ pterii.pl
@@ -11,6 +11,7 @@ while ($code =~ m/
                  ([-+*\/%><!.^=|&~:?A-LN-RT-XZ,;()\[\]{} ]      # single char
                  | [fhjkmnqtvwxyz]|[a-z].                       # function
                  | (["']) (?:\\.|(?!\2).)*\2                    # quote
+                 | [0-9]+                                       # number
                  | M(.)   (?:\\.|(?!\3).)*\3                    # m regex
                  | [SY](.)(?:\\.|(?!\4).)*\4(?:(?!\4).|\\.)*\4  # s, y regex
                  | [\$@#](?:[A-Za-z]+|.)                        # variable
@@ -19,4 +20,38 @@ while ($code =~ m/
     push @tokens, $1;
 }
 
-say foreach @tokens;
+# the stack (1-letter variable name in case someone wants to use it in the
+# Pterii code
+my @s = ();
+# args used by almost every single operator/function
+my @args = ();
+# "grab args" -- update the @args variable
+sub ga {
+    # TODO cut off at last stack div
+    @args = @s;
+    @s = ();
+}
+
+my $comptoken = sub {
+    my $token = shift;
+
+    # let's handle easy constants first
+    my $constmap = {
+        '+' => 'ga; push(@s, 0); $s[-1] += $_ foreach @args;',
+        '-' => 'ga; push(@s, shift @args); $s[-1] -= $_ foreach @args;',
+        '*' => 'ga; push(@s, 0); $s[-1] *= $_ foreach @args;',
+        '/' => 'ga; push(@s, shift @args); $s[-1] /= $_ foreach @args;',
+        ' ' => '',
+        'sa' => 'ga; say @args;',
+    }->{$_};
+    return $constmap if $constmap;
+
+    # ok so it's more complicated than that ;(
+    if ($token =~ /^[0-9]+$/) {
+        return "push(\@s, $token);";
+    }
+};
+
+my $compiled = join(' ', map $comptoken->($_), @tokens);
+eval $compiled;
+print @s;
